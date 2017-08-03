2bfe221875ed829c85f973cb8c50b22c644e6219
richhall <rhllrichard@gmail.com>
2017-08-03 17:03:47 +0100

    initial commit

---
+++ data.js
@@ -0,0 +1,336 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-the-start",
+      "name": "The Start",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.4982280389994305,
+          "pitch": -0.01725895807945399,
+          "rotation": 4.71238898038469,
+          "target": "1-the-crescent"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.0946155565805391,
+          "pitch": 0.03196048631390447,
+          "title": "Possible augment",
+          "text": "to test support for augmented reality. &nbsp;e.g. Memorial to the freedom from slavery of sentient machines!&nbsp;"
+        }
+      ]
+    },
+    {
+      "id": "1-the-crescent",
+      "name": "The Crescent",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -0.7975546295495803,
+        "pitch": -0.0769013596139132,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.7421672331816129,
+          "pitch": 0.0024758691593937954,
+          "rotation": 1.5707963267948966,
+          "target": "2-clarkson-mermorial"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-clarkson-mermorial",
+      "name": "Clarkson Mermorial",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 0.5373139389710495,
+        "pitch": -0.042570860488488904,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.5406980842800841,
+          "pitch": -0.032432210532991945,
+          "rotation": 0,
+          "target": "3-north-brink-passage-to-exchange-square"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.6494308979104737,
+          "pitch": 0.5806529870082944,
+          "title": "Potential Augment",
+          "text": "e.g. show futuristic underground infrastructure"
+        },
+        {
+          "yaw": 1.9694354573767816,
+          "pitch": 0.1550764315772355,
+          "title": "Possible augment",
+          "text": "Futuristic underground infrastructure"
+        },
+        {
+          "yaw": -2.9642274535636233,
+          "pitch": -0.3532563055781939,
+          "title": "Clarkson Memorial",
+          "text": "Text"
+        },
+        {
+          "yaw": -3.0313251930095717,
+          "pitch": 0.24656915141056857,
+          "title": "Stand on steps and look down",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "3-north-brink-passage-to-exchange-square",
+      "name": "North Brink passage to exchange square",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 2.768932559160924,
+        "pitch": -0.028690281654487393,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.6999500205754554,
+          "pitch": -0.0007723080558950102,
+          "rotation": 0,
+          "target": "4-lloyds-bank"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -3.097599875782393,
+          "pitch": -0.06954797976677796,
+          "title": "Not a good place for augment",
+          "text": "I nearly got run over by a cyclist taking this pic!"
+        }
+      ]
+    },
+    {
+      "id": "4-lloyds-bank",
+      "name": "Lloyds Bank",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 3.0261832875856065,
+        "pitch": 0.1397677026724935,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.1371041394052472,
+          "pitch": 0.11253650180434427,
+          "rotation": 0,
+          "target": "5-peckover-house"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.8858816373902219,
+          "pitch": -0.07219986242624721,
+          "title": "intended augment point across the road",
+          "text": "<span style=\"font-size: 16px; background-color: rgba(103, 115, 131, 0.8);\">I missed this beyond the parked cars.</span>"
+        }
+      ]
+    },
+    {
+      "id": "5-peckover-house",
+      "name": "Peckover House",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 1.8768529938493197,
+        "pitch": 0.13866339988258147,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.5894819403653022,
+          "pitch": 0.0264468771655757,
+          "rotation": 0,
+          "target": "6-the-first-tower"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.3875628179556863,
+          "pitch": -0.02599442422307341,
+          "title": "intended augment point - lamp post",
+          "text": "Text"
+        },
+        {
+          "yaw": 2.544113109068804,
+          "pitch": -0.3885132258698505,
+          "title": "Possible alternative augment",
+          "text": "in event that street furniture (lamp posts etc) are not possible."
+        }
+      ]
+    },
+    {
+      "id": "6-the-first-tower",
+      "name": "The first tower",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 0.35434099072541336,
+        "pitch": 0.09070031803725165,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": -3.0064671253940745,
+          "pitch": -0.10559013050231236,
+          "title": "Augment 4",
+          "text": "Tower"
+        }
+      ]
+    }
+  ],
+  "name": "The Brink",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
